b4938be2f51ee404aa6a89ac9a4a4b33e53e72b2
jonathan-j-choi <jonathan.junghoon.choi@gmail.com>
2023-01-13 09:38:55 -0500

    removed GA

--- public/java/java.js
+++ public/java/java.js
@@ -1,13 +1,3 @@
 $('.carousel').carousel({
   interval: 4000
 })
-
-/**
-* Example of pushing WURFL capabilities to google analytics
-*/
-ga('send', 'pageview', {
-    'dimension1': WURFL.complete_device_name,
-    'dimension2': WURFL.form_factor,
-    'dimension3': WURFL.is_mobile,
-    'dimension4': WURFL.is_robot
-});
